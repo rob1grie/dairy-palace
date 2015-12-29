973a41a3ae685f605a457db573b745ca6e10b01b
Rob G <rob@lgd-it.us>
2015-12-29 16:18:44 -0600

    After creating database with Netbeans, tried creating it programmatically, then creating a table, but DB Visualizer doesn't show the Users table that should have been created.

--- src/model/Database.java
+++ src/model/Database.java
@@ -68,100 +68,100 @@ public class Database {
 				+ "position_id INT NOT NULL)";
 		stmt.executeUpdate(sql);
 
-		// Positions table
-		sql = "CREATE TABLE IF NOT EXISTS POSITIONS "
-				+ "(id INTEGER PRIMARY KEY, "
-				+ "position TEXT NOT NULL);";
-		stmt.executeUpdate(sql);
-
-		sql = "CREATE TABLE IF NOT EXISTS PERMISSIONS "
-				+ "(id INTEGER PRIMARY KEY, "
-				+ "user_id INT NOT NULL, "
-				+ "view_change BOOL NOT NULL, "
-				+ "reports BOOL NOT NULL, "
-				+ "orders BOOL NOT NULL, "
-				+ "food_expense BOOL NOT NULL, "
-				+ "utilities BOOL NOT NULL);";
-		stmt.executeUpdate(sql);
-
-		sql = "CREATE TABLE IF NOT EXISTS PREFERENCES "
-				+ "(id INTEGER PRIMARY KEY, "
-				+ "mainFrameWidth INT NOT NULL, "
-				+ "mainFrameHeight INT NOT NULL, "
-				+ "selectedView TEXT NOT NULL);";
-		stmt.executeUpdate(sql);
-
-		sql = "CREATE TABLE IF NOT EXISTS SHIFT_DATAS "
-				+ "(id INTEGER PRIMARY KEY, "
-				+ "shift INT NOT NULL, "
-				+ "date TEXT NOT NULL, "
-				+ "user_id INT NOT NULL, "
-				+ "food REAL NOT NULL DEFAULT 0.0, "
-				+ "rest_supp REAL NOT NULL DEFAULT 0.0, "
-				+ "off_supp REAL NOT NULL DEFAULT 0.0, "
-				+ "rep_maint REAL NOT NULL DEFAULT 0.0, "
-				+ "freight REAL NOT NULL DEFAULT 0.0, "
-				+ "cred_cards REAL NOT NULL DEFAULT 0.0, "
-				+ "store_cash REAL NOT NULL DEFAULT 0.0, "
-				+ "z_dept_tl REAL NOT NULL DEFAULT 0.0, "
-				+ "overrings REAL NOT NULL DEFAULT 0.0, "
-				+ "beg_cash REAL NOT NULL DEFAULT 0.0, "
-				+ "z_tx REAL NOT NULL DEFAULT 0.0, "
-				+ "z_coupon REAL NOT NULL DEFAULT 0.0, "
-				+ "school_charges REAL NOT NULL DEFAULT 0.0, "
-				+ "tax_exempt_sales REAL NOT NULL DEFAULT 0.0, "
-				+ "donations REAL NOT NULL DEFAULT 0.0, "
-				+ "gift_certs REAL NOT NULL DEFAULT 0.0, "
-				+ "ecards REAL NOT NULL DEFAULT 0.0, "
-				+ "discounts REAL NOT NULL DEFAULT 0.0, "
-				+ "mgr_on_duty TEXT NOT NULL DEFAULT '');";
-		stmt.executeUpdate(sql);
-
-		sql = "CREATE TABLE IF NOT EXISTS OTHER_PAID_OUTS "
-				+ "(id INTEGER PRIMARY KEY, "
-				+ "shift_data_id INT NOT NULL, "
-				+ "label TEXT NOT NULL DEFAULT '', "
-				+ "cost REAL NOT NULL DEFAULT 0.0);";
-		stmt.executeUpdate(sql);
-
-		sql = "CREATE TABLE IF NOT EXISTS EMPLOYEES "
-				+ "(id INTEGER PRIMARY KEY, "
-				+ "emp_id INT NOT NULL, "
-				+ "first_name TEXT NOT NULL, "
-				+ "last_name TEXT NOT NULL);";
-		stmt.executeUpdate(sql);
-
-		sql = "CREATE TABLE IF NOT EXISTS REGISTER_AUDITS "
-				+ "(id INTEGER PRIMARY KEY, "
-				+ "date_time TEXT NOT NULL, "
-				+ "shift INT NOT NULL, "
-				+ "tape_read REAL NOT NULL, "
-				+ "cash_count REAL NOT NULL, "
-				+ "audit INT NOT NULL, "
-				+ "register TEXT NOT NULL, "
-				+ "manager_id TEXT NOT NULL);";
-		stmt.executeUpdate(sql);
-
-		sql = "CREATE TABLE IF NOT EXISTS REGISTER_AUDIT_EMPLOYEE "
-				+ "(id INTEGER PRIMARY KEY, "
-				+ "audit_id INT NOT NULL, "
-				+ "employee_id INT NOT NULL);";
-		stmt.executeUpdate(sql);
-
-		sql = "CREATE TABLE IF NOT EXISTS VENDORS "
-				+ "(id INTEGER PRIMARY KEY, "
-				+ "name TEXT NOT NULL);";
-		stmt.executeUpdate(sql);
-
-		sql = "CREATE TABLE IF NOT EXISTS VENDOR_INVOICES "
-				+ "(id INTEGER PRIMARY KEY, "
-				+ "date TEXT NOT NULL, "
-				+ "manager_id INT NOT NULL, "
-				+ "vendor_id INT NOT NULL, "
-				+ "invoice_num TEXT NOT NULL, "
-				+ "amount REAL NOT NULL, "
-				+ "include_in_food BOOL NOT NULL DEFAULT TRUE);";
-		stmt.executeUpdate(sql);
+//		// Positions table
+//		sql = "CREATE TABLE IF NOT EXISTS POSITIONS "
+//				+ "(id INTEGER PRIMARY KEY, "
+//				+ "position TEXT NOT NULL);";
+//		stmt.executeUpdate(sql);
+//
+//		sql = "CREATE TABLE IF NOT EXISTS PERMISSIONS "
+//				+ "(id INTEGER PRIMARY KEY, "
+//				+ "user_id INT NOT NULL, "
+//				+ "view_change BOOL NOT NULL, "
+//				+ "reports BOOL NOT NULL, "
+//				+ "orders BOOL NOT NULL, "
+//				+ "food_expense BOOL NOT NULL, "
+//				+ "utilities BOOL NOT NULL);";
+//		stmt.executeUpdate(sql);
+//
+//		sql = "CREATE TABLE IF NOT EXISTS PREFERENCES "
+//				+ "(id INTEGER PRIMARY KEY, "
+//				+ "mainFrameWidth INT NOT NULL, "
+//				+ "mainFrameHeight INT NOT NULL, "
+//				+ "selectedView TEXT NOT NULL);";
+//		stmt.executeUpdate(sql);
+//
+//		sql = "CREATE TABLE IF NOT EXISTS SHIFT_DATAS "
+//				+ "(id INTEGER PRIMARY KEY, "
+//				+ "shift INT NOT NULL, "
+//				+ "date TEXT NOT NULL, "
+//				+ "user_id INT NOT NULL, "
+//				+ "food REAL NOT NULL DEFAULT 0.0, "
+//				+ "rest_supp REAL NOT NULL DEFAULT 0.0, "
+//				+ "off_supp REAL NOT NULL DEFAULT 0.0, "
+//				+ "rep_maint REAL NOT NULL DEFAULT 0.0, "
+//				+ "freight REAL NOT NULL DEFAULT 0.0, "
+//				+ "cred_cards REAL NOT NULL DEFAULT 0.0, "
+//				+ "store_cash REAL NOT NULL DEFAULT 0.0, "
+//				+ "z_dept_tl REAL NOT NULL DEFAULT 0.0, "
+//				+ "overrings REAL NOT NULL DEFAULT 0.0, "
+//				+ "beg_cash REAL NOT NULL DEFAULT 0.0, "
+//				+ "z_tx REAL NOT NULL DEFAULT 0.0, "
+//				+ "z_coupon REAL NOT NULL DEFAULT 0.0, "
+//				+ "school_charges REAL NOT NULL DEFAULT 0.0, "
+//				+ "tax_exempt_sales REAL NOT NULL DEFAULT 0.0, "
+//				+ "donations REAL NOT NULL DEFAULT 0.0, "
+//				+ "gift_certs REAL NOT NULL DEFAULT 0.0, "
+//				+ "ecards REAL NOT NULL DEFAULT 0.0, "
+//				+ "discounts REAL NOT NULL DEFAULT 0.0, "
+//				+ "mgr_on_duty TEXT NOT NULL DEFAULT '');";
+//		stmt.executeUpdate(sql);
+//
+//		sql = "CREATE TABLE IF NOT EXISTS OTHER_PAID_OUTS "
+//				+ "(id INTEGER PRIMARY KEY, "
+//				+ "shift_data_id INT NOT NULL, "
+//				+ "label TEXT NOT NULL DEFAULT '', "
+//				+ "cost REAL NOT NULL DEFAULT 0.0);";
+//		stmt.executeUpdate(sql);
+//
+//		sql = "CREATE TABLE IF NOT EXISTS EMPLOYEES "
+//				+ "(id INTEGER PRIMARY KEY, "
+//				+ "emp_id INT NOT NULL, "
+//				+ "first_name TEXT NOT NULL, "
+//				+ "last_name TEXT NOT NULL);";
+//		stmt.executeUpdate(sql);
+//
+//		sql = "CREATE TABLE IF NOT EXISTS REGISTER_AUDITS "
+//				+ "(id INTEGER PRIMARY KEY, "
+//				+ "date_time TEXT NOT NULL, "
+//				+ "shift INT NOT NULL, "
+//				+ "tape_read REAL NOT NULL, "
+//				+ "cash_count REAL NOT NULL, "
+//				+ "audit INT NOT NULL, "
+//				+ "register TEXT NOT NULL, "
+//				+ "manager_id TEXT NOT NULL);";
+//		stmt.executeUpdate(sql);
+//
+//		sql = "CREATE TABLE IF NOT EXISTS REGISTER_AUDIT_EMPLOYEE "
+//				+ "(id INTEGER PRIMARY KEY, "
+//				+ "audit_id INT NOT NULL, "
+//				+ "employee_id INT NOT NULL);";
+//		stmt.executeUpdate(sql);
+//
+//		sql = "CREATE TABLE IF NOT EXISTS VENDORS "
+//				+ "(id INTEGER PRIMARY KEY, "
+//				+ "name TEXT NOT NULL);";
+//		stmt.executeUpdate(sql);
+//
+//		sql = "CREATE TABLE IF NOT EXISTS VENDOR_INVOICES "
+//				+ "(id INTEGER PRIMARY KEY, "
+//				+ "date TEXT NOT NULL, "
+//				+ "manager_id INT NOT NULL, "
+//				+ "vendor_id INT NOT NULL, "
+//				+ "invoice_num TEXT NOT NULL, "
+//				+ "amount REAL NOT NULL, "
+//				+ "include_in_food BOOL NOT NULL DEFAULT TRUE);";
+//		stmt.executeUpdate(sql);
 
 		stmt.close();
 		db.con.close();
