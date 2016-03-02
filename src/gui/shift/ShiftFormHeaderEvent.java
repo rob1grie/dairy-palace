84349d903c455c141f51647e1df8e57f10b731cf
Rob Grieshaber <rob16grie@yahoo.com>
2016-03-02 10:47:25 -0600

    Added ShiftFormHeaderListener, moving towards adding the event listener

---
+++ src/gui/shift/ShiftFormHeaderEvent.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.shift;
+
+import java.util.EventObject;
+
+/**
+ *
+ * @author rob
+ */
+public class ShiftFormHeaderEvent extends EventObject {
+
+	private String buttonName;
+	
+	public ShiftFormHeaderEvent(Object source) {
+		super(source);
+	}
+	
+	public ShiftFormHeaderEvent(Object source, String name) {
+		super(source);
+		
+		this.buttonName = name;
+	}
+
+	public String getButtonName() {
+		return buttonName;
+	}
+
+	public void setButtonName(String buttonName) {
+		this.buttonName = buttonName;
+	}
+	
+}
