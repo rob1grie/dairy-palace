655cc0a383e847df7bee1343d55344df59978a66
Rob Grieshaber <rob16grie@yahoo.com>
2016-04-12 10:45:09 -0500

    Removed unused include

--- src/gui/shift/ShiftToolbar.java
+++ src/gui/shift/ShiftToolbar.java
@@ -8,7 +8,6 @@ package gui.shift;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
